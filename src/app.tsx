a434e4387cd8a0b0f86e94ed9e95ba4c09b71e5e
bitfarmer01 <idlirocker@gmail.com>
2023-10-07 19:40:52 -0400

    Added color mode and svg files

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,18 @@
 import { Button, Container } from "@chakra-ui/react"
 import { Route, Router } from "preact-router"
 import UserPage from "./Pages/UserPage"
-import Home from "./Pages/Home"
 import PostPage from "./Pages/PostPage"
+import Header from "./components/Header"
 
 const app = () => {
 	return (
 		<div>
-			<Router>
-				<Route path="/home" component={Home} />
-				<Route path="/:username" component={UserPage} />
-				<Route path="/:username/post/pid" component={PostPage} />
-			</Router>
-
 			<Container maxW="620px">
-				<Button>Hyello</Button>
+				<Header />
+				<Router>
+					<Route path="/:username" component={UserPage} />
+					<Route path="/:username/post/pid" component={PostPage} />
+				</Router>
 			</Container>
 		</div>
 	)
